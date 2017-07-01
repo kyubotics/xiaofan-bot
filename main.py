9b176fb91277eb26ab578948da8ac8a117b7c07d
Richard Chien <richardchienthebest@gmail.com>
2017-07-01 14:05:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ import json
 import sys
 import requests
 
-json_file = sys.argv[1]
-token = sys.argv[2]
+data_file = sys.argv[1]
 
-with open(json_file, 'r', encoding='utf-8') as f:
-    event = json.loads(f.read())
+with open(data_file, 'r', encoding='utf-8') as f:
+    data = json.loads(f.read())
+    
+token = data['token']
+event = data['event']
+maker_webhook = data['maker_webhook']
 
-if event['post_type'] == 'message':
-    requests.get('https://ali-east-1.r-c.im:5701/send_private_msg',
-                 headers={'Authorization': 'token ' + token},
-                 params={'user_id': event['user_id'], 'message': event['message']})
+if event['post_type'] != 'message':
+    exit(0)
+
+message = event['message']
+message_str = ''
+image_urls = []
+for part in message:
+    if part['type'] == 'text':
+        message_str += ' ' + part['data']['text']
+    elif part['type'] == 'image':
+        url = part['data'].get('url')
+        if url:
+            image_urls.append(url)
+
+requests.get('https://ali-east-1.r-c.im:5701/send_private_msg',
+             headers={'Authorization': 'token ' + token},
+             params={'user_id': event['user_id'], 'message': message_str})
