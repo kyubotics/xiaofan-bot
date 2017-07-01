7fcca305588280622db02c97b563b4816a5bf3f1
Richard Chien <richardchienthebest@gmail.com>
2017-07-01 16:44:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,27 @@ data_file = sys.argv[1]
 with open(data_file, 'r', encoding='utf-8') as f:
     data = json.loads(f.read())
 
+value1 = data['value1']  # event from CoolQ
+value2 = data['value2']  # API call from IFTTT
+value3 = data['value3']  # unused
 super_id = data['super_id']
 token = data['token']
-event = data['event']
 maker_webhook = data['maker_webhook']
 
-if event['post_type'] != 'message':
-    exit(0)
-
-message = event['message']
-message_str = ''
-for part in message:
-    if part['type'] == 'text':
-        message_str += ' ' + part['data']['text']
-
-message_str = message_str.strip()
-sp = re.split('\s|:|：', message_str, maxsplit=1)
-if len(sp) < 2:
-    # not a command, skip it
-    exit(0)
-
-cmd = sp[0]
-msg = sp[1].strip()
-
-if cmd == '发推':
-    if str(event['user_id']) != str(super_id):
+if value1:
+    try:
+        e = json.loads(value1)
+        import event
+        event.handle(e)
         exit(0)
-    requests.post(maker_webhook % 'send_tweet', json={'value1': msg})
+    except json.decoder.JSONDecodeError:
+        pass
+
+if value2:
+    try:
+        a = json.loads(value2)
+        import api_call
+        api_call.handle(a)
+        exit(0)
+    except json.decoder.JSONDecodeError:
+        pass
